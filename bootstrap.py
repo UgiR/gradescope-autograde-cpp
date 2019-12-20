b6f054082e6874fb808f9a8e950cc232e1f39a97
Ugnius Rumsevicius <33397860+UgiR@users.noreply.github.com>
2019-12-19 23:42:50 -0600

    Execute make for catch

--- bootstrap.py
+++ bootstrap.py
@@ -1,3 +1,5 @@
 #  Code to be executed before the tests are run
 def init():
-    pass
+    #  3. compile Catch once so we are not recompiling for each test
+    #     the resulting .o object file will be use for all tests
+    subprocess.call(['make', 'catch-essential'], cwd='/autograder/source/tests')
