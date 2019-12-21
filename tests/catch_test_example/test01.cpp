f17e121aec1c407f6c0992d7885377c6dae3c277
Ugnius Rumsevicius <ugniusrumsevicius@gmail.com>
2019-12-20 21:01:59 -0600

    Include simple example tests

---
+++ tests/catch_test_example/test01.cpp
@@ -0,0 +1,8 @@
+#include "student_file.h"
+#include "catch.hpp"
+
+TEST_CASE( "Test 01", "Example Project" )
+{
+  int x = 2 + 3;
+  REQUIRE(x == 5);
+}
---
+++ tests/valgrind_test_example/test02.cpp
@@ -0,0 +1,7 @@
+#include "student_file.h"
+#include "catch.hpp"
+
+TEST_CASE( "Test 02", "Example Project" )
+{
+  int *arr = new int[10];
+}
