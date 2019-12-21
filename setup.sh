f2deaa412e60b52ec2453cb0c5254fba82085e4a
Ugnius Rumsevicius <ugniusrumsevicius@gmail.com>
2019-12-21 11:58:17 -0600

    Add cppcheck example

--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ apt-get install valgrind -y
 # Install Catch Framework for C++ testing
 apt-get install catch -y
 
+# Install cppcheck for static code analysis
+apt-get install cppcheck -y
+
 # Move python uploaded grader files into the same directory as run_autograder
 cp /autograder/source/grade_util.py /autograder
 cp /autograder/source/bootstrap.py /autograder
